5157e413288b88be50e36c273f6d93f6a088944d
chjuca <chjuca@utpl.edu.ec>
2021-12-05 10:42:18 -0500

    Adding create teacher function

--- functions/controllers/teacher.controller.js
+++ functions/controllers/teacher.controller.js
@@ -1,11 +1,27 @@
+const admin = requiere("firebase-admin")
+
+admin.initializeApp({
+    credential: admin.credential.cert("../credentials.json"),
+    databaseURL: "https://lazarillo-challenge-9e1c9-default-rtdb.firebaseio.com"
+  
+});
+
+const db = admin.firestore();
 
 async function getTeachers(req, res) {
-try {
-    
-} catch (error) {
-    return res.status(500).json({
-        ok: false,
-        message: "Something goes wrong"
-    })
-}  
+    const {name, subjects, dateBirth, direction, typeContract}= req.body;
+    try {
+        db.collection("teachers").add({
+            name,
+            subjects,
+            dateBirth,
+            direction,
+            typeContract
+        })       
+    } catch (error) {
+        return res.status(500).json({
+            ok: false,
+            message: "Something goes wrong"
+        })
+    }  
 }
--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,8 @@
 const functions = require("firebase-functions");
-const admin = requiere("firebase-admin")
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
 app.use(cors());
-
-admin.initializeApp({
-    credential: admin.credential.cert("./credendials.json"),
-    databaseURL: "https://lazarillo-challenge-9e1c9-default-rtdb.firebaseio.com"
-  
-});
-
-app.get('/holis', (req, res) => {
-    res.json({
-        message: "Hola"
-    })
-})
 
 exports.app = functions.https.onRequest(app);
