08d32baee7aac5345bf94f30c8f71b867c1ff53e
chjuca <chjuca@utpl.edu.ec>
2021-12-05 10:33:13 -0500

    adding connection to firebase database

---
+++ functions/controllers/teacher.controller.js
@@ -0,0 +1,11 @@
+
+async function getTeachers(req, res) {
+try {
+    
+} catch (error) {
+    return res.status(500).json({
+        ok: false,
+        message: "Something goes wrong"
+    })
+}  
+}
--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,21 @@
 const functions = require("firebase-functions");
+const admin = requiere("firebase-admin")
+const express = require("express");
+const cors = require("cors");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+app.use(cors());
+
+admin.initializeApp({
+    credential: admin.credential.cert("./credendials.json"),
+    databaseURL: "https://lazarillo-challenge-9e1c9-default-rtdb.firebaseio.com"
+  
+});
+
+app.get('/holis', (req, res) => {
+    res.json({
+        message: "Hola"
+    })
+})
+
+exports.app = functions.https.onRequest(app);
